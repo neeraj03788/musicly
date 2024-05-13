1b66bd4d8b002eba5e010c9270aa297c979f7b08
Neeraj <neerajkumar03788@gmail.com>
2024-05-13 23:20:42 +0530

    FeatureCard and Sticky Scroll

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,6 @@
+import FeaturedCourses from "@/components/FeaturedCourses";
 import HeroSection from "@/components/HeroSection";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 
 
@@ -6,7 +8,8 @@ export default function Home() {
   return (
   <main className="min-h-screen  bg-black/[0.96] antialiased bg-grid-white/[0.02]">
     <HeroSection />
-    
+    <FeaturedCourses />
+    <WhyChooseUs />
     </main>
   );
 }
